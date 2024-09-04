7828d02ea0a24fc475656b267bc2ddb80173bbb2
kebabzter <81696183+kebabzter@users.noreply.github.com>
2024-09-04 20:17:26 +0200

    Profile page additions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./src/pages/*.{html,js}"],
-  safelist:[
-    "underline",
-    "decoration-2",
-    "underline-offset-8"
-  ],
-  theme: {
-    extend: {
-        fontFamily:{
-            rubik: ['Rubik', 'sans-serif']
+    content: ["./*.{html,js}", "./src/pages/*.{html,js}"],
+    safelist: ["underline", "decoration-2", "underline-offset-8"],
+    theme: {
+        extend: {
+            fontFamily: {
+                rubik: ["Rubik", "sans-serif"],
+            },
+            fontSize: {
+                "10xl": "10rem",
+                "11xl": "12rem",
+                megaxl: "15rem",
+                gigaxl: "30rem",
+            },
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-3deg)" },
+                    "50%": { transform: "rotate(3deg)" },
+                },
+            },
+            animation: {
+                "spin-slow": "spin 5s linear infinite",
+                "spin-slower": "spin 10s linear infinite",
+                "spin-slowerer": "spin 15s linear infinite",
+                "bounce-slow": "bounce 5s infinite",
+                wiggle: "wiggle 1s ease-in-out infinite",
+            },
         },
-        fontSize:{
-            "10xl": "10rem",
-            "11xl": "12rem",
-            "gigaxl": "30rem"
-        },
-        animation:{
-            'spin-slow': 'spin 5s linear infinite',
-            'spin-slower': 'spin 10s linear infinite',
-            'spin-slowerer': 'spin 15s linear infinite',
-        }
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
