4e46e319368d68f0daccf7c160e4819d54b73887
kebabzter <81696183+kebabzter@users.noreply.github.com>
2024-10-17 22:04:44 +0200

    Blog posts now all complete

--- src/scripts/blogpost.js
+++ src/scripts/blogpost.js
@@ -1,12 +1,3 @@
-window.onload = loadPost();
+import {openAside} from "./lib.js"
 
-export async function loadPost() {
-    
-    const path = window.location.pathname;
-    const fileName = path.substring(path.lastIndexOf('/') + 1);
-    const postName = fileName.replace('.html', '.md');
-    const response = await fetch(`./${postName}`);
-    const markdown = await response.text();
-    const htmlContent = marked(markdown); // Convert Markdown to HTML
-    document.getElementById('blog-content').innerHTML = htmlContent;
-};
+openAside();
